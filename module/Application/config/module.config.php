818bd3cb512dc3d238ebcf9fc73a7e59429cb97d
Kristopher Wilson <kristopherwilson@gmail.com>
2014-12-28 12:50:11 -0500

    restyling + cleaning up ZF2 application

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -8,50 +8,50 @@
  */
 
 return array(
-    'router' => array(
-        'routes' => array(
-            'home' => array(
+    'router' => [
+        'routes' => [
+            'home' => [
                 'type' => 'Zend\Mvc\Router\Http\Literal',
-                'options' => array(
+                'options' => [
                     'route'    => '/',
-                    'defaults' => array(
+                    'defaults' => [
                         'controller' => 'Application\Controller\Index',
                         'action'     => 'index',
-                    ),
-                ),
-            ),
-            // The following is a route to simplify getting started creating
-            // new controllers and actions without needing to create a new
-            // module. Simply drop new controllers in, and you can access them
-            // using the path /application/:controller/:action
-            'application' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    'route'    => '/application',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
-                        'controller'    => 'Index',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        ),
-    ),
+                    ],
+                ],
+            ],
+            'customers' => [
+                'type' => 'Segment',
+                'options' => [
+                    'route'    => '/customers',
+                    'defaults' => [
+                        'controller' => 'Application\Controller\Customers',
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+            'orders' => [
+                'type' => 'Segment',
+                'options' => [
+                    'route'    => '/orders',
+                    'defaults' => [
+                        'controller' => 'Application\Controller\Orders',
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+            'invoices' => [
+                'type' => 'Segment',
+                'options' => [
+                    'route'    => '/invoices',
+                    'defaults' => [
+                        'controller' => 'Application\Controller\Invoices',
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
     'service_manager' => array(
         'abstract_factories' => array(
             'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
